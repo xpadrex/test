fe362bedd84c8dc849bffcdc1788ed44051eff11
xpadrex <xpadrex@gmail.com>
2018-07-17 14:10:30 -0400

    Works but error with sting input

--- file.c
+++ file.c
@@ -17,17 +17,37 @@ void load_objects()
   printf("Loading from file...\n");
   char line[250];
   int i = 0;
+  int n;
+  char *d;
+  char *t;
+  char *l;
+  int v;
+  int dam;
+  int arm;
 
-  FILE *in;
-  in = fopen("objects.dat", "r");
+  FILE *in = fopen("objects.dat", "r");
 
-  fscanf(in, "%s,%s,%s,%d,%d,%d", objects[i].description, objects[i].tag,
+  n = fscanf(in, "%s,%s,%s,%d,%d,%d ", d,t,l,&v,&dam,&arm);
+
+      /*
+                                objects[i].description, objects[i].tag,
                                 objects[i].location, &objects[i].value, 
                                 &objects[i].damage, &objects[i].armour);
-  printf("File Contents: %s, %s, %s, %d, %d, %d.\n", 
+       */
+  //printf("File Contents: %s, %s, %s, %d, %d, %d.\n", d,t,l,v,dam,arm);
+      /*
                                 objects[i].description, objects[i].tag,
                                 objects[i].location, objects[i].value,
                                 objects[i].damage, objects[i].armour);
+
+       */
+  printf("  d = %s\n", d);
+  printf("  t = %s\n", t);
+  printf("  l = %s\n", l);
+  printf("  v = %d\n", v);
+  printf("dam = %d\n", dam);
+  printf("arm = %d\n", arm);
+  printf("n = %d\n", n);
   i++;
   fclose(in);
 
