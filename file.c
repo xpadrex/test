3bf4d8b159dd9aed6ad4147fcba5c47faf58515f
xpadrex <xpadrex@gmail.com>
2018-07-17 22:27:40 -0400

    Code for reading file now working

--- file.c
+++ file.c
@@ -29,34 +29,22 @@ void load_objects()
 
   FILE *in = fopen("objects.dat", "r");
 
-  while (fgets(line, sizeof(line), in) != NULL)
-  {
-    strcpy(description, strtok(line, ","));
-    strcpy(tag, strtok(NULL, ","));
-    strcpy(location, strtok(NULL, ","));
-    strcpy(value, strtok(NULL, ","));
-    strcpy(damage, strtok(NULL, ","));
-    strcpy(armour, strtok(NULL, ","));
+  while (fgets(line, sizeof(line), in) != NULL) {
+    strcpy(description, strtok(line, ",\n"));
+    strcpy(tag, strtok(NULL, ",\n"));
+    strcpy(location, strtok(NULL, ",\n"));
+    strcpy(value, strtok(NULL, ",\n"));
+    strcpy(damage, strtok(NULL, ",\n"));
+    strcpy(armour, strtok(NULL, ",\n"));
     
-    if (value != "") {
-      objects[i].description = description;
-      objects[i].tag = tag;
-      objects[i].location = location;
-      objects[i].value = atoi(value);
-      objects[i].damage = atoi(damage);
-      objects[i].armour = atoi(armour);
-    }
-    else {
-      fclose(in);
-      return;
-    }
+    objects[i].description = description;
+    objects[i].tag = tag;
+    objects[i].location = location;
+    objects[i].value = atoi(value);
+    objects[i].damage = atoi(damage);
+    objects[i].armour = atoi(armour);
 
-    printf("%s,%s,%s,%d,%d,%d.\n", 
-        /*
-        description, tag, location,
-        value, damage, armour);
-        */
-        objects[i].description,
+    printf("%s,%s,%s,%d,%d,%d.\n", objects[i].description,
         objects[i].tag, objects[i].location, objects[i].value, 
         objects[i].damage, objects[i].armour);
     i++;
