1f68ccf73a922b885282e87db9c589b3b8d919e7
Andre <xpadrex@gmail.com>
2018-07-17 21:35:39 -0400

    Revised using strtok

--- file.c
+++ file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *description;
@@ -10,45 +11,56 @@ typedef struct {
     int armour;
 } object;
 
-object objects[1];
+object objects[100];
 
 void load_objects()
 {
   printf("Loading from file...\n");
   char line[250];
+  char description[200];
+  char tag[15];
+  char location[15];
+  char value[10];
+  char damage[10];
+  char armour[10];
+
   int i = 0;
   int n;
-  char *d;
-  char *t;
-  char *l;
-  int v;
-  int dam;
-  int arm;
 
   FILE *in = fopen("objects.dat", "r");
 
-  n = fscanf(in, "%s,%s,%s,%d,%d,%d ", d,t,l,&v,&dam,&arm);
+  while (fgets(line, sizeof(line), in) != NULL)
+  {
+    strcpy(description, strtok(line, ","));
+    strcpy(tag, strtok(NULL, ","));
+    strcpy(location, strtok(NULL, ","));
+    strcpy(value, strtok(NULL, ","));
+    strcpy(damage, strtok(NULL, ","));
+    strcpy(armour, strtok(NULL, ","));
+    
+    if (value != "") {
+      objects[i].description = description;
+      objects[i].tag = tag;
+      objects[i].location = location;
+      objects[i].value = atoi(value);
+      objects[i].damage = atoi(damage);
+      objects[i].armour = atoi(armour);
+    }
+    else {
+      fclose(in);
+      return;
+    }
 
-      /*
-                                objects[i].description, objects[i].tag,
-                                objects[i].location, &objects[i].value, 
-                                &objects[i].damage, &objects[i].armour);
-       */
-  //printf("File Contents: %s, %s, %s, %d, %d, %d.\n", d,t,l,v,dam,arm);
-      /*
-                                objects[i].description, objects[i].tag,
-                                objects[i].location, objects[i].value,
-                                objects[i].damage, objects[i].armour);
-
-       */
-  printf("  d = %s\n", d);
-  printf("  t = %s\n", t);
-  printf("  l = %s\n", l);
-  printf("  v = %d\n", v);
-  printf("dam = %d\n", dam);
-  printf("arm = %d\n", arm);
-  printf("n = %d\n", n);
-  i++;
+    printf("%s,%s,%s,%d,%d,%d.\n", 
+        /*
+        description, tag, location,
+        value, damage, armour);
+        */
+        objects[i].description,
+        objects[i].tag, objects[i].location, objects[i].value, 
+        objects[i].damage, objects[i].armour);
+    i++;
+  }
   fclose(in);
 
   return;
